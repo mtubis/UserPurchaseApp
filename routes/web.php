aeeb483f27e558d04f4b64d5ffbfc0c6f4ac966a
Magdalena Tubis <magda.tubis@gmail.com>
2024-07-03 01:35:53 +0200

    Route for users with last purchases

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/users/last-purchase-date', [UserController::class, 'usersWithLastPurchaseDate']);
+
