21317a7699af8b1d5b27926f915ee0d4e884e3a4
Magdalena Tubis <magda.tubis@gmail.com>
2024-07-03 03:20:44 +0200

    Naming routes

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 Route::get('/', [UserController::class, 'index'])->name('home');
-
-Route::get('/users/last-purchase-date', [UserController::class, 'usersWithLastPurchaseDate']);
-
-Route::get('/users/sorted-by-birthday', [UserController::class, 'usersSortedByBirthday']);
-
-Route::get('/users/birthdays-this-week', [UserController::class, 'usersWithBirthdaysThisWeek']);
+Route::get('/users/last-purchase-date', [UserController::class, 'usersWithLastPurchaseDate'])->name('users.last_purchase_date');
+Route::get('/users/sorted-by-birthday', [UserController::class, 'usersSortedByBirthday'])->name('users.sorted_by_birthday');
+Route::get('/users/birthdays-this-week', [UserController::class, 'usersWithBirthdaysThisWeek'])->name('users.birthdays_this_week');
 
